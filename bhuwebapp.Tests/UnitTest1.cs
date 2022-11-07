158ff1b6098064d97713560a2978db5e95112b3a
Genry Leyva <gleyvaglez@gmail.com>
2022-11-07 12:47:34 -0300

    unit test

---
+++ bhuwebapp.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using bhuwebapp.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace bhuwebapp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string StringExpected = "11/7/2022 12:00:00 AM";
+        private const long EpocExpected = 1667779200;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = Dateutils.DateAsString(new DateTime(2022, 11, 07));
+            Assert.AreEqual(StringExpected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = Dateutils.DateAsTimestamp(new DateTime(2022, 11, 07));
+            Assert.AreEqual(EpocExpected, result);
+        }
+    }
+}
